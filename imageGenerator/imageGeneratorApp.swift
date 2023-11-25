9cbf8dafeb97df2f51d6db39c6a02adb1790e338
nurKholis <nur.kholis@phincon.com>
2023-11-25 13:27:23 +0700

    Initial Commit

---
+++ imageGenerator/imageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageGeneratorApp.swift
+//  imageGenerator
+//
+//  Created by Phincon on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct imageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
